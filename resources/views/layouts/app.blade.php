b849ef65dafc8de3a12c4d9ad401abedd7564fb6
Erickmart13 <erick_mart13@hotmail.com>
2024-08-16 14:15:44 -0500

    Layout app ok, dashboard argon ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -33,4 +33,75 @@
             </main>
         </div>
     </body>
+</html> --}}
+
+
+<!--
+
+=========================================================
+* Argon Dashboard 2 Tailwind - v1.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-tailwind
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+* Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/medicina-icon.png')}}" />
+    <link rel="icon" type="image/png" href="{{asset('img/medicina-icon.png')}}" />
+    <title>Sistema Hospitalario Isidro Ayora</title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet">
+    <!-- Nucleo Icons -->
+    <link href="{{asset('css/nucleo-icons.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/nucleo-svg.css')}}" rel="stylesheet" />
+    <!-- Popper -->
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <!-- Main Styling -->
+    @vite('resources/css/argon-dashboard-tailwind.css')
+  </head>
+
+  <body class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
+    <div class="absolute w-full bg-blue-panel dark:hidden min-h-75"></div>
+
+    <!-- sidenav  -->
+    @include('layouts.menu')
+
+    <!-- end sidenav -->
+
+    <main class="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl">
+ <!-- Navbar -->
+ @include('layouts.navigation1')
+ <!-- end Navbar -->
+ 
+ <div class="w-full px-6 py-6 mx-auto"> 
+
+    @yield('content')
+
+
+<!-- footer -->
+@include('layouts.footer')
+<!-- end footer -->
+</div>
+
+    </main>
+    @include('layouts.configurator')
+  </body>
+  <!-- plugin for charts  -->
+@vite('resources/js/plugins/chartjs.min.js')
+<!-- plugin for scrollbar  -->
+@vite('resources/js/plugins/perfect-scrollbar.min.js')
+<!-- main script file  -->
+@vite('resources/js/argon-dashboard-tailwind.js')
 </html>
--- resources/views/layouts/navigation1.blade.php
+++ resources/views/layouts/navigation1.blade.php
@@ -8,7 +8,7 @@
                             <a class="text-white opacity-50" href="javascript:;">Pages</a>
                         </li>
                         <li class="text-sm pl-2 capitalize leading-normal text-white before:float-left before:pr-2 before:text-white before:content-['/']"
-                            aria-current="page">Dashboard</li>
+                            aria-current="page">@yield('page-title','Bienvenidos')</li>
                     </ol>
                     <h4 class="mb-0 font-normal text-white capitalize">Sistema Hospitalario Isidro Ayora</h4>
                 </nav>
@@ -69,7 +69,42 @@
 
                               
                           </ul>
+                          
                       </li>
+                      <!-- Settings Dropdown -->
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>{{ Auth::user()->name }}</div>
+
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            </div>
+
                         <li class="flex items-center pl-4 xl:hidden">
                             <a href="javascript:;" class="block p-0 text-sm text-white transition-all ease-nav-brand"
                                 sidenav-trigger>
@@ -104,14 +139,14 @@
                                         href="javascript:;">
                                         <div class="flex py-1">
                                             <div class="my-auto">
-                                                <img src="../assets/img/team-2.jpg"
+                                                <img src="{{asset('img/team-2.jpg')}}"
                                                     class="inline-flex items-center justify-center mr-4 text-sm text-white h-9 w-9 max-w-none rounded-xl" />
                                             </div>
                                             <div class="flex flex-col justify-center">
                                                 <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white">
                                                     <span class="font-semibold">New message</span> from Laur</h6>
                                                 <p class="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
-                                                    <i class="mr-1 fa fa-clock"></i>
+                                                    <i class="mr-1 fas fa-clock"></i>
                                                     13 minutes ago
                                                 </p>
                                             </div>
@@ -124,7 +159,7 @@
                                         href="javascript:;">
                                         <div class="flex py-1">
                                             <div class="my-auto">
-                                                <img src="../assets/img/small-logos/logo-spotify.svg"
+                                                <img src="{{asset('img/small-logos/logo-spotify.svg')}}"
                                                     class="inline-flex items-center justify-center mr-4 text-sm text-white bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 h-9 w-9 max-w-none rounded-xl" />
                                             </div>
                                             <div class="flex flex-col justify-center">
@@ -139,46 +174,7 @@
                                     </a>
                                 </li>
 
-                                <li class="relative">
-                                    <a class="dark:hover:bg-slate-900 ease py-1.2 clear-both block w-full whitespace-nowrap rounded-lg px-4 transition-colors duration-300 hover:bg-gray-200 hover:text-slate-700"
-                                        href="javascript:;">
-                                        <div class="flex py-1">
-                                            <div
-                                                class="inline-flex items-center justify-center my-auto mr-4 text-sm text-white transition-all duration-200 ease-nav-brand bg-gradient-to-tl from-slate-600 to-slate-300 h-9 w-9 rounded-xl">
-                                                <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    xmlns:xlink="http://www.w3.org/1999/xlink">
-                                                    <title>credit-card</title>
-                                                    <g stroke="none" stroke-width="1" fill="none"
-                                                        fill-rule="evenodd">
-                                                        <g transform="translate(-2169.000000, -745.000000)"
-                                                            fill="#FFFFFF" fill-rule="nonzero">
-                                                            <g transform="translate(1716.000000, 291.000000)">
-                                                                <g transform="translate(453.000000, 454.000000)">
-                                                                    <path class="color-background"
-                                                                        d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"
-                                                                        opacity="0.593633743"></path>
-                                                                    <path class="color-background"
-                                                                        d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z">
-                                                                    </path>
-                                                                </g>
-                                                            </g>
-                                                        </g>
-                                                    </g>
-                                                </svg>
-                                            </div>
-                                            <div class="flex flex-col justify-center">
-                                                <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white">
-                                                    Payment successfully completed</h6>
-                                                <p
-                                                    class="mb-0 text-xs leading-tight text-slate-400 dark:text-white/80">
-                                                    <i class="mr-1 fa fa-clock"></i>
-                                                    2 days
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
+                                
                             </ul>
                         </li>
                     </ul>
